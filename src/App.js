781c7235f656ddfd0860ce91821801802c7acee1
Sepehr Dehghani <sepehr.dh99@gmail.com>
2023-02-02 20:56:54 +0330

    implement header HTML

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ function App() {
   return (
     <div id="home-page">
       <section className="full-container">
-        <nav></nav>
+        <nav>
+          <div className="logo">
+            <a>AirBNB</a>
+          </div>
+          <div className="menu">
+            <a>places to stay</a>
+            <a>experiences</a>
+            <a>online experiences</a>
+          </div>
+          <div className="tools">
+            <a>Become a host</a>
+          </div>
+        </nav>
         <div className="search-bar"></div>
         <div className="content"></div>
       </section>
